e6eefd2d492dab1c226f1b6fc8ce623ebd649422
Solomon S. <solomon.sefiw@berhanbanksc.com>
2025-07-14 18:57:38 +0300

    config updater

--- CMS.Persistance/Configuration/DocumentConfiguration.cs
+++ CMS.Persistance/Configuration/DocumentConfiguration.cs
@@ -1,19 +1,4 @@
-﻿using CMS.Domain.Document;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace CMS.Persistance.Configuration
-{
-    public class DocumentConfiguration : IEntityTypeConfiguration<Document>
-    {
-        public void Configure(EntityTypeBuilder<Document> builder)
-        {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).HasDefaultValueSql("NEWID()");
-        }
-    }
-}
-//using CMS.Domain.Document;
+﻿//using CMS.Domain.Document;
 //using Microsoft.EntityFrameworkCore;
 //using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -24,9 +9,24 @@ namespace CMS.Persistance.Configuration
 //        public void Configure(EntityTypeBuilder<Document> builder)
 //        {
 //            builder.HasKey(x => x.Id);
-
-//            // ✅ Use gen_random_uuid() and cast to text for PostgreSQL
-//            builder.Property(x => x.Id).HasDefaultValueSql("gen_random_uuid()::text");
+//            builder.Property(x => x.Id).HasDefaultValueSql("NEWID()");
 //        }
 //    }
 //}
+using CMS.Domain.Document;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CMS.Persistance.Configuration
+{
+    public class DocumentConfiguration : IEntityTypeConfiguration<Document>
+    {
+        public void Configure(EntityTypeBuilder<Document> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            // ✅ Use gen_random_uuid() and cast to text for PostgreSQL
+            builder.Property(x => x.Id).HasDefaultValueSql("gen_random_uuid()::text");
+        }
+    }
+}
--- CMS.Persistance/Configuration/EmployeeConfiguration.cs
+++ CMS.Persistance/Configuration/EmployeeConfiguration.cs
@@ -1,30 +1,4 @@
-﻿using CMS.Domain.Employee;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace CMS.Persistance.Configuration
-{
-    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
-    {
-        public void Configure(EntityTypeBuilder<Employee> builder)
-        {
-            //builder.ToTable(x => x.IsTemporal(t =>
-            //{
-            //    t.HasPeriodStart("PeriodStart");
-            //    t.HasPeriodEnd("PeriodEnd");
-            //}));
-            builder.Property(x => x.EmployeeId)
-                .IsRequired()
-                .HasDefaultValueSql("NEXT VALUE FOR EMPLOYEEID");
-            builder.HasIndex(X => X.EmployeeId).IsUnique(true).IsClustered(false);
-            builder.HasOne(x => x.BusinessUnits).WithMany().HasForeignKey(x => x.BusinessUnitID);
-            builder.HasOne(x => x.Job).WithMany().HasForeignKey(x => x.JobId).OnDelete(DeleteBehavior.NoAction);
-        }
-    }
-}
-
-
-//using CMS.Domain.Employee;
+﻿//using CMS.Domain.Employee;
 //using Microsoft.EntityFrameworkCore;
 //using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -34,22 +8,48 @@ namespace CMS.Persistance.Configuration
 //    {
 //        public void Configure(EntityTypeBuilder<Employee> builder)
 //        {
-
-//            Removed SQL Server - specific default value
+//            //builder.ToTable(x => x.IsTemporal(t =>
+//            //{
+//            //    t.HasPeriodStart("PeriodStart");
+//            //    t.HasPeriodEnd("PeriodEnd");
+//            //}));
 //            builder.Property(x => x.EmployeeId)
-//                .IsRequired(); // No default value, handled manually or via app logic
-
-//            Removed IsClustered(not supported by PostgreSQL)
-//            builder.HasIndex(x => x.EmployeeId).IsUnique();
-
-//            builder.HasOne(x => x.BusinessUnits)
-//                   .WithMany()
-//                   .HasForeignKey(x => x.BusinessUnitID);
-
-//            builder.HasOne(x => x.Job)
-//                   .WithMany()
-//                   .HasForeignKey(x => x.JobId)
-//                   .OnDelete(DeleteBehavior.NoAction);
+//                .IsRequired()
+//                .HasDefaultValueSql("NEXT VALUE FOR EMPLOYEEID");
+//            builder.HasIndex(X => X.EmployeeId).IsUnique(true).IsClustered(false);
+//            builder.HasOne(x => x.BusinessUnits).WithMany().HasForeignKey(x => x.BusinessUnitID);
+//            builder.HasOne(x => x.Job).WithMany().HasForeignKey(x => x.JobId).OnDelete(DeleteBehavior.NoAction);
 //        }
 //    }
 //}
+
+
+using CMS.Domain.Employee;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CMS.Persistance.Configuration
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+
+           // Removed SQL Server - specific default value
+            builder.Property(x => x.EmployeeId)
+                .IsRequired(); // No default value, handled manually or via app logic
+
+           // Removed IsClustered(not supported by PostgreSQL)
+            builder.HasIndex(x => x.EmployeeId).IsUnique();
+
+            builder.HasOne(x => x.BusinessUnits)
+                   .WithMany()
+                   .HasForeignKey(x => x.BusinessUnitID);
+
+            builder.HasOne(x => x.Job)
+                   .WithMany()
+                   .HasForeignKey(x => x.JobId)
+                   .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
