5002f0b41c333b1b3ed1d88dd42c2056749469e2
Solomon S. <solomon.sefiw@berhanbanksc.com>
2025-07-17 17:11:23 +0300

    Late Pub

--- CMS.Web/src/app/api/emptySplitApi.ts
+++ CMS.Web/src/app/api/emptySplitApi.ts
@@ -5,7 +5,8 @@ import { addNotification } from "../../features/notification";
 export const emptySplitApi = createApi({
   reducerPath: "HCMSApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://cms-test.somee.com",
+    baseUrl: "https://amhara-cms.somee.com",
+         credentials: 'include', // ✅ include cookies if applicable
     prepareHeaders: (headers) => {
       if (!headers.has("Content-Type")) {
         headers.set("Content-Type", "application/json");
