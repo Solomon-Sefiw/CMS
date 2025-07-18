26d5669a50ea11da18344fdc18e35a9aac15465c
Solomon S. <solomon.sefiw@berhanbanksc.com>
2025-07-18 13:28:55 +0300

    test

--- CMS.Web/src/features/user/mfa.tsx
+++ CMS.Web/src/features/user/mfa.tsx
@@ -34,7 +34,7 @@ export const MFA = () => {
       verify({
         verificationCode: {
           code: values.code,
-          email, // ✅ Add email to payload
+     //     email, // ✅ Add email to payload
         },
       })
         .unwrap()
